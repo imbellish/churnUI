e81fd2d37b78ea4b609e010d6a839b1288569048
Ian Bellamy <bellamy.ian.m@gmail.com>
2015-05-16 15:00:46 -0600

    configured for testing

---
+++ e2e-tests/conf.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ianbellamy on 5/16/15.
+
+ */
+// conf.js
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js']
+}
---
+++ e2e-tests/spec.js
@@ -0,0 +1,8 @@
+// spec.js
+describe('Protractor Demo App', function() {
+    it('should have a title', function() {
+        browser.get('http://localhost:8000/');
+
+        expect(browser.getTitle()).toEqual('ChurnOver | Butter Location Research');
+    });
+});
