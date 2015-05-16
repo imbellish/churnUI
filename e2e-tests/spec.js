a97f9ee1bb7e7e1af92e421cf6a7e67c20b4bafc
Ian Bellamy <bellamy.ian.m@gmail.com>
2015-05-16 16:11:36 -0600

    more testing

--- e2e-tests/spec.js
+++ e2e-tests/spec.js
@@ -2,15 +2,28 @@
 describe('ChurnOver Application', function() {
     it('should have a title', function() {
         browser.get('http://localhost:8000/');
-
         expect(browser.getTitle()).toEqual('ChurnOver | Butter Location Research');
     });
-    it('should show different search parameters', function(){
-        var address = element(by.buttonText("Search By Address"));
-        var content = webdriver.By.id("content-body");
-        address.click();
 
-        //expect(content.contains())
+    it('should show menu options', function(){
+        element.all(by.css('.menuoptions')).then(function(items){
+            expect(items.length).toBe(5);
+        });
+    });
+
+    it('should change content tab when menu is clicked', function(){
+        element.all(by.css('.menuoptions')).then(function(items){
+            items[1].click();
+
+        })
+    });
+
+    it('content tabs whould change when menu clicked', function(){
+        var address = element(by.css("#content_tab_002"));
+        address.click();
+        var content = element(by.css(".byaddress"));
+        expect(content).isDisplayed();
 
     })
+
 });
