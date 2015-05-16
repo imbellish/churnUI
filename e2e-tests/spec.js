7ad5a7bb53bc31fe5d5ced5d2ff2a83706f85f42
Ian Bellamy <bellamy.ian.m@gmail.com>
2015-05-16 15:25:26 -0600

    logo arranged left, add tests

--- e2e-tests/spec.js
+++ e2e-tests/spec.js
@@ -1,8 +1,16 @@
 // spec.js
-describe('Protractor Demo App', function() {
+describe('ChurnOver Application', function() {
     it('should have a title', function() {
         browser.get('http://localhost:8000/');
 
         expect(browser.getTitle()).toEqual('ChurnOver | Butter Location Research');
     });
+    it('should show different search parameters', function(){
+        var address = element(by.buttonText("Search By Address"));
+        var content = webdriver.By.id("content-body");
+        address.click();
+
+        //expect(content.contains())
+
+    })
 });
