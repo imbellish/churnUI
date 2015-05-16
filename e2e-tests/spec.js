49ebd12dc5a53e64a80716aa2683f9f74c551127
Ian Bellamy <bellamy.ian.m@gmail.com>
2015-05-16 17:17:00 -0600

    passing tests

--- e2e-tests/spec.js
+++ e2e-tests/spec.js
@@ -8,17 +8,23 @@ function sleepFor( sleepDuration ){
 
 describe('ChurnOver Application', function() {
     it('should have a title', function() {
-        browser.get('http://localhost:8000/');
+        // run http-server -a localhost -p 8000 on the /app/ directory
+        browser.get('http://localhost:8000/index2.html');
         expect(browser.getTitle()).toEqual('ChurnOver | Butter Location Research');
     });
 
     it('should show menu options', function(){
+        element(by.css("#tab_002")).click();
+        element(by.css('.menu')).click();
+        //sleepFor(100);
         element.all(by.css('.menuoptions')).then(function(items){
             expect(items.length).toBe(5);
         });
     });
 
     it('should change content tab when menu is clicked', function(){
+
+
         var expectedClassnames = [
             'home',
             'byaddress',
@@ -30,7 +36,11 @@ describe('ChurnOver Application', function() {
             for (i = 0; i < items.length; i ++){
                 items[i].click();
                 sleepFor(50);
+                //element(by.css(".logo img")).click();
+                //element(by.css('.menu')).click();
                 expect(element(by.css("."+expectedClassnames[i]))).toBeTruthy();
+                //element(by.css('.menu')).click();
+
             }
         });
     });
